37e7dadc33cad8316f9f5660c0a1ccab9044c38f
jaspersorriso <s10149149@connect.np.edu.sg>
2015-12-04 11:58:02 +0800

    Change port to 7777 but allow change of port on 3rd arg

--- app/server.js
+++ app/server.js
@@ -1,7 +1,10 @@
 var express = require('express');
-
+var port = process.argv[2] | 7777;
 var app = express();
 
 app.use(express.static(__dirname+'/app'));
 
-app.listen(8888);
+console.log("Server hosted on port: "+port);
+
+app.listen(port);
+
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var port = process.argv[2] || 7777;
 var app = express();
 
 app.use(express.static(__dirname+'/app'));
 
 app.use('/bower_components',express.static(__dirname+ "/bower_components"));
 
-app.listen(8888);
+app.listen(port);
+
+console.log("Server hosted on port: "+port);
